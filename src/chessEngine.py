e04368a1107fda3e8bac12e808f8ce9b60d92512
Sashibhushan rajput <sam556913@gmail.com>
2021-10-03 01:34:38 +0530

    Initialising the project, Creating chess board.

---
+++ src/chessEngine.py
@@ -0,0 +1,25 @@
+"""
+    This class will be responsible for storing all the information about the current state of the chess game.
+    It will also generate all the valid moves from the current state.
+    It will also contain a log of all the moves played till the current state.
+"""
+
+
+class GameState():
+    def __init__(self):
+        # The chessboard is an 8 by 8 2D list, each element of the list has 2 characters
+        # A sqaure with a chess piece on it is represented by a string of two alphabets while an empty square with no piece is represented by the string "--"
+        # The first character of a chess piece represents the color of the piece 'w' - white, 'b' - black
+        # And the second character represents the type of the piece 'p' - pawn, 'R' - rook, 'N' - Knight, 'B' - Bishop 'Q' - Queen, 'K' - King
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["bR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.movesLog = []
---
+++ src/chessMain.py
@@ -0,0 +1,9 @@
+"""
+    This is our main interaction file, which will be responsible for handling user Input and displaying the current GameState Object.
+"""
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQUARE_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 18
+IMAGES = {}
